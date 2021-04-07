3bc14328abc02679ec456a654075d76544972877
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-07 13:34:12 +0000

    broke out client functions into client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+ const connect = function() {
+  const toServer = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+
+  });
+  // interpret incoming data as text
+  toServer.setEncoding('utf8'); 
+
+  toServer.on('data', (data) => {
+    console.log('Message from server: ', data)
+  });
+
+  return toServer;
+}
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const net = require('net');
+const connect = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const toServer = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-
-  });
-  // interpret incoming data as text
-  toServer.setEncoding('utf8'); 
-
-  toServer.on('data', (data) => {
-    console.log('Message from server: ', data)
-  });
-
-  return toServer;
-}
 
 console.log('Connecting ...');
 connect();
